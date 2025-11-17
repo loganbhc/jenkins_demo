5da4eb5c1802fab8cce7f3a2480c3e390f03fcff
loganbhc <52465039+loganbhc@users.noreply.github.com>
2025-11-17 15:59:04 +0530

    Update TestingController.java

--- src/main/java/com/example/DemoForJenkins/controller/TestingController.java
+++ src/main/java/com/example/DemoForJenkins/controller/TestingController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestingController {
     @GetMapping
     public String health(){
-        return "I am Healthy";
+        return "Hello Everyone, I am Healthy";
     }
 }
